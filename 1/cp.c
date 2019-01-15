d85bbe41ab532646cd38e69551180f765a0d1f70
Yousef Jarrar <005845836@coyote.csusb.edu>
2019-01-15 11:17:31 -0800

    final revision

--- 1/cp.c
+++ 1/cp.c
@@ -1,30 +1,51 @@
-#include "type.s"
+/***************************************************************************************************
+ * cp.c
+ * By: Yousef Jarrar and Jose Perez
+ * CSE 461 Lab 1
+ **************************************************************************************************/
+#include "types.h"
 #include "stat.h"
 #include "user.h"
 #include "fcntl.h"
 
 char buf[512];
- int main(int argc, char *argv[]) {
- int fd0, fd1, n;
-
+int main(int argc, char *argv[]) {
+  int fd0, fd1, fd2, n;
   if (argv <= 2) {
     printf(1, "Need 2 Arguements!\n");
     exit();
-    }
-for (int i =2; i<=argc; i++){
-  if((fd0 = open(argv[1], O_RDONLY))<0) {
+  }
+  
+  for (int i =2; i<=argc; i++){
+    //opens README file
+    if((fd0 = open(argv[1], O_RDONLY))<0) {
       printf(1, "cp: Cannot Open %s\n", argv[1]);
       exit();
     }
-  if((fd1 = open(argv[2], O_CREATE|O_RDWR))<0) {
-        printf(1, "cp: Cannot Open %s\n", argv[2]);
-        exit();
-      }
-  while((n = read ( fd0, buf, sizeof(buf)))>0){
-    write (fd1, buf, n);
+
+    //opens myFile1
+    if((fd1 = open(argv[2], O_CREATE|O_RDWR))<0) {
+      printf(1, "cp: Cannot Open %s\n", argv[2]);
+      exit();
+    }
+    
+    //opens the myFile2
+    if((fd2 = open(argv[3], O_CREATE|O_RDWR))<0) {
+      printf(1, "cp: Cannot Open %s\n", argv[3]);
+      exit();
+    }
+    
+    while((n = read ( fd0, buf, sizeof(buf)))>0){
+      //writes onto the myFiles
+      write (fd1, buf, n);
+      write (fd2, buf, n);
+    }
+    //closes README File
+    close(fd0);
+
+    //closes myFile1 and myFile2
+    close(fd1);
+    close(fd2);
   }
-  close(fd0);
-  close(fd1);
+  exit();
 }
-exit();
-  }
